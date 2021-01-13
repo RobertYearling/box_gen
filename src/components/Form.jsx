2cdc9a815ddfbab2f93f09831229e514b879143f
Robert Yearling <laserguy@mac.com>
2021-01-12 20:24:10 -0600

    All Files FIrst Upload

---
+++ src/components/Form.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import styles from './Form.module.css';
+
+const Form = () => {
+
+    const [formColor, setFormColor] = useState({
+        color: "",
+    })
+
+    const [colorList, setColorList] = useState([])
+
+    const changeHandler = (e) => {
+        setFormColor({
+            ...formColor,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const subHandler = (e) => {
+        e.preventDefault()
+        setColorList([...colorList, formColor]);
+        setFormColor({
+            color: "",
+        })
+    }
+
+    return (
+        <>
+            <div>
+                <form onSubmit={subHandler}>
+                    <h3>Color: <input type="text" name="color" id="" onChange={changeHandler} /></h3>
+                    <p><input type="submit" value="Add" /></p>
+                </form>
+            </div>
+            <div>
+            {
+                colorList.map((box) => {
+                    return <div className={styles.newBox} style={{backgroundColor: box.color}}></div>
+                })
+            }
+            </div>
+        </>
+    );
+};
+
+export default Form;
